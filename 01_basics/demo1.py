88125b40340d2448bef03df081a821e93128f836
rakshithasetty41 <rakshithasetty41@gmail.com>
2025-09-10 16:28:28 +0530

    identifiers

--- 01_basics/demo1.py
+++ 01_basics/demo1.py
@@ -1,3 +1,41 @@
 a=10
 b=20
 print(a+b)
+print(9)
+# We used script mode to execute below code
+
+# Syntax (Rules)
+print("good morning")
+#print(good morning) in python rule is, if you have text data then we need to enclose that in "" or ''
+hello = "hello hi"
+print(hello)
+
+print("hello")
+# name is identifier 
+
+# class = "10th Standard" # SyntaxError: invalid syntax --> class is a keyword
+classes = "10th Standard"
+print(classes)
+
+print("class")
+
+# Identifiers
+data = "hello good morning"
+print(data)
+# 2day_data = "hello" # invalid as starts with digit
+# print(2day_data)
+_data = "hello"
+print(_data)
+
+# $data = "hello" # SyntaxError: invalid syntax cannot have $ only _ allowed
+# print($data)
+
+# Improper way of using Identifiers
+x = "Ravi"
+y = 30
+z = "Python"
+
+# Proper way of using Identifiers
+student_name = "Ravi"
+student_name = 30
+student_enrolled_course = "python"

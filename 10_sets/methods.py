12a5c75e89792efda31f0dfa16c5d65a16893879
rakshithasetty41 <rakshithasetty41@gmail.com>
2025-09-24 16:19:53 +0530

    sets

--- 10_sets/methods.py
+++ 10_sets/methods.py
@@ -101,6 +101,9 @@ s2 = {40,50,60,70,80}
 print(s1.intersection_update(s2))
 print(s1)
 print(s2)
+print(s2.intersection_update(s1))
+print(s1)
+print(s2)
 
 
 #difference:remove the  common elements
@@ -108,6 +111,9 @@ s1={10,20,30,40,50}
 s2={40,50,60,70,80}
 print(s1-s2)
 print(s1.difference(s2))
+print(s2.difference(s1))
+print(s2-s1)\
+
 print(s1)
 print(s2)
 
@@ -115,6 +121,9 @@ print(s2)
 s1={10,20,30,40,50}
 s2={40,50,60,70,80}
 print(s1.difference_update(s2))
+print(s1)
+print(s2)
+print(s2.difference_update(s1))
 print(s1)
 print(s2)
 
@@ -132,6 +141,10 @@ s2 = {40,50,60,70,80}
 print(s1.symmetric_difference_update(s2))
 print(s1)
 print(s2)
+print(s2.symmetric_difference_update(s1))
+print(s1)
+print(s2)
+
 
 #subset:check if the given set is subset of another set
 s1={10,20,30,40,50}
@@ -144,6 +157,7 @@ print(s3.issubset(s1))
 #superset:check if given set is a superset of another set
 print(s1.issuperset(s2))
 print(s1.issuperset(s3))
+print(s2.issuperset(s1))
 
 #disjoint:check if two sets has no common elements
 s4={90,100}

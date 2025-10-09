60cb302e59c70612e5bdc607344c2e8aa0038e96
rakshithasetty41 <rakshithasetty41@gmail.com>
2025-10-09 13:39:26 +0530

    client.py

---
+++ client.py
@@ -0,0 +1,6 @@
+# Client needs custom math package 
+from math_packages import add,mul
+print("value of var",add.var)
+print("sum of nums",add.add_fun(10,5))
+print("value of var",mul.var)
+print("product of nums",mul.mul_fun(10,5))

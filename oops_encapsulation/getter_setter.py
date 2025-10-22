3ba448210eb30821addb6c946fa72a456a356b33
rakshithasetty41 <rakshithasetty41@gmail.com>
2025-10-22 21:23:23 +0530

    getter and setter

---
+++ oops_encapsulation/getter_setter.py
@@ -0,0 +1,25 @@
+# With Encapsulation & With Setter - Getter
+class student():
+    def __init__(self,name,marks):
+       self.__name=name #private
+       self.__marks=marks #private
+
+    def display_info(self):
+        print(f"{self.__name} has {self.__marks} marks")
+
+          
+    # setter 
+    def set_marks(self,marks):
+        if 0 <= marks <=100:
+            self.__marks = marks
+        else:
+            print("Invalid Marks, only 0-100 allowed")
+                
+    # getter 
+    def get_marks(self):
+        return self.__marks
+    
+s1 = student("ravi",95)  
+s1.display_info() 
+s1.set_marks(200)
+print(s1.get_marks())

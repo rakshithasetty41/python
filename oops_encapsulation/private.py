31027e35fbf1e87f0a5e63aeace32345de26e58d
rakshithasetty41 <rakshithasetty41@gmail.com>
2025-10-22 21:23:41 +0530

    private

---
+++ oops_encapsulation/private.py
@@ -0,0 +1,16 @@
+#private
+
+class A:
+    def __init__(self,a,b):
+        self.__a=a#private
+        self.__b=b#private
+
+obj=A(10,20)
+print(obj._A__a)
+print(obj._A__b)
+
+print(obj.a) # not accessible 
+print(obj.b) # not accessible 
+
+# print(obj._A__a) # accessing via name mangling 
+# obj._MyClass__private_variable

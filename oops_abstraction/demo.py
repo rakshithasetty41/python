5a87e5eabbda6f5e0a0a08c0b5f71995382d8957
rakshithasetty41 <rakshithasetty41@gmail.com>
2025-11-02 18:54:35 +0530

    abstraction

---
+++ oops_abstraction/demo.py
@@ -0,0 +1,39 @@
+# Abstraction
+
+# Laptop Manufacturer 
+
+# Laptop Contract / Interface (govt said these are must for laptop)
+
+# Without Abstraction
+
+# concrete class 
+class Laptop:
+    #concrete methods
+    def processor(self):
+        print("Laptop processor")
+    def ram_hdd(self):
+        print("Laptop has a ram and hdd")
+    
+
+class Lenovo(Laptop):
+    
+    def processor(self):
+        print("Lenovo Laptop Processor ") 
+        
+class Dell(Laptop):
+    
+    def ram_hdd(self):
+        print("Dell Laptop RAM & HDD")
+        
+# End user 
+print("Buying Lenovo Laptop")    
+lenovo = Lenovo()
+lenovo.processor()
+
+print("Buying Dell Laptop") 
+dell = Dell()
+dell.ram_hdd()
+
+laptop = Laptop()
+laptop.processor()
+    

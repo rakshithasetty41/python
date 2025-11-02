35a051bda6ec3be298c6aa26d79f7994ca033919
rakshithasetty41 <rakshithasetty41@gmail.com>
2025-11-02 18:55:03 +0530

    overload

---
+++ oops_polymorphism/overload.py
@@ -0,0 +1,19 @@
+#traditional overlading
+class mathops:
+    def add(self,a,b):
+        return a+b
+    def add(self,a,b,c):# overwrites the above method 
+        return a+b+c
+    
+obj=mathops()
+print(obj.add(10,20,30))
+
+# Handling Overloading Python way 
+class Maths:
+    def add(self,a=0,b=0,c=0): # overwrites the above method 
+        return a + b + c
+    
+new_obj = Maths()
+print(new_obj.add(10))
+print(new_obj.add(10,20))
+print(new_obj.add(10,20,30))    
